32f42a5cba6d7f92a8268f0ae5a519202b586ed1
Sofia <65925889+sofiaoferrari@users.noreply.github.com>
2023-10-29 23:50:55 -0300

    Entrega

--- traceroute.py
+++ traceroute.py
@@ -1,37 +1,35 @@
-from scapy.all import * 
-import sys 
+from scapy.all import *
+import sys, time
 
 def traceroute(host):
-    max_hops = 30  # Número máximo de saltos
-    ttl = 0
-
+    max_hops = 30  # Establezco el número máximo de saltos, en este caso 30
+    ttl = 0     
     print(f"Rastreando la ruta hacia {host}:")
     dest_ip = socket.gethostbyname(host)
 
     while ttl <= max_hops:
-        # Construye un paquete ICMP Echo Request con el valor de TTL actual
+        # Calculo el tiempo inicial antes de enviar el paquete 
+        start_time = time.time()
+        # Construye un paquete ICMP con el valor de TTL actual y el host pasado por consola
         packet = IP(dst=host, ttl=ttl) / ICMP(type=8, code=0)
-
-        # Envía el paquete y espera una respuesta o un timeout
         reply = sr1(packet, timeout=10)
 
         if reply is None:
-            # Si no se recibe respuesta, muestra un asterisco
-            print(f"{ttl}: No recibi respuesta ")
+            print(f"{ttl}: No recibí respuesta ")
         else:
-            # Si se recibe una respuesta, muestra la dirección IP del host
-            print(f"Numero de ttl : {ttl}, IP del host : {reply.src}")
+            # Si se recibe una respuesta, muestra la dirección IP del host y el RTT
+            end_time = time.time()
+            rtt = (end_time - start_time) * 1000  # Calculo el RTT en milisegundos
+            print(f"Numero de ttl : {ttl}, IP del host : {reply.src}, RTT: {rtt:.2f} ms")
 
             if reply.src == dest_ip:
-                # Hemos llegado al host de destino
-                print(f"Hemos llegado al host {host} destino, su IP es : {reply.src}")
+                print(f"Hemos llegado al host destino : {reply.src}")
                 break
-
         ttl += 1
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Ejecuto: python3 traceroute.py <host>")
+        print("La cantidad de argumentos no es correcta")
         sys.exit(1)
 
     host = sys.argv[1]
